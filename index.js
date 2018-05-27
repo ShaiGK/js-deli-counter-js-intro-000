42c60c42f00e627173e25b5865a002393881549f
Shai Goldberg-Kellman <sgoldbergkellman@gmail.com>
2018-05-27 21:46:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ function takeANumber (deliLine,custName) {
   deliLine.push (custName)
   return `Welcome, ${custName}. You are number ${deliLine.indexOf(custName) + 1} in line.`
 }
+
+function nowServing (array) {
+  var arr = []
+  arr.push(array[0])
+  array.shift()
+  return `Currently serving ${arr}.`
+}
